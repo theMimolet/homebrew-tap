647374b5dd13592376eb928c0713635923a0d73a
KLAUSS Samuel <samuel.klauss@ynov.com>
2026-03-12 21:06:22 +0100

    refactor: added spaces + local-linux prototype

--- Casks/font-luciole-math.rb
+++ Casks/font-luciole-math.rb
@@ -1,11 +1,11 @@
 cask "font-luciole-math" do
-  version :latest
-  sha256 :no_check
+    version :latest
+    sha256 :no_check
 
-  url "https://luciole-vision.com/fonts/Luciole-Math.zip",
-      verified:  "luciole-vision.com"
-  name "Luciole Math"
-  homepage "https://luciole-vision.com/en/math.html"
+    url "https://luciole-vision.com/fonts/Luciole-Math.zip",
+        verified:  "luciole-vision.com"
+    name "Luciole Math"
+    homepage "https://luciole-vision.com/en/math.html"
 
-  font "Luciole-Math.otf"
+    font "Luciole-Math.otf"
 end
